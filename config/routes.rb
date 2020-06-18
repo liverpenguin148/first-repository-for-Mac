e584dbd5351de6e3be5548658dd6fb40520d8c79
takahiro.usui <liverpenguin1408@gmail.com>
2020-06-18 10:36:26 +0900

    tasksルーティングの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callback: 'omniauth_callbacks' }
   root 'pages#index'
   get 'pages/show'
+  resources :tasks, only: [:create, :destroy]
 end
