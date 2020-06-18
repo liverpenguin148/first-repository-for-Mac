8cb3380cf480183de2038b14f0423b8b6feacbb9
takahiro.usui <liverpenguin1408@gmail.com>
2020-06-18 10:01:25 +0900

    userモデルのusernameのバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+	validates :username, presence: true
+	validates :username, length: { minimum: 4 }
+	validates :username, length: { maximum: 12 }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
